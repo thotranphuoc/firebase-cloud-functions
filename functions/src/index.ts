f1b2fdef15cdd40a73ef538c47a2655a2c4f82ef
mac <mac@Jade-MBP.net.fpt>
2019-01-12 00:06:31 +0700

    receive http trigger onRequest, read database from firestore, reponse with result

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,26 @@
 import * as functions from 'firebase-functions';
 
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-    console.log(request);
-    response.send("Hello from Firebase!");
-});
+// onRequest function
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//     console.log(request);
+//     response.send("Hello from Firebase!");
+// });
+
+// firestore
+export const getBostonWeather = functions.https.onRequest((request, response) => {
+    admin.firestore().doc('cities-weather/boston-ma-us').get()
+        .then((snapDoc) => {
+            const data = snapDoc.data();
+            response.send(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            response.status(500).send(err);
+        })
+})
